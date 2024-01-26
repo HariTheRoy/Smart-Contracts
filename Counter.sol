c1bdb930bdac3fcc05e10b885894ba577dcfbf2b
HariTheRoy <130649213+HariTheRoy@users.noreply.github.com>
2024-01-26 12:29:52 +0530

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+contract Count{
+    address public owner;
+
+    struct Counter{
+        uint number;
+        string description;
+    }
+
+    Counter counter;
+
+    modifier  onlyOwner(){
+        require(msg.sender == owner, "only the owner can increment and decreament the counter");
+        _;
+    }
+    
+    constructor(uint intial_value, string memory description){
+        owner ==msg.sender;
+        counter = Counter(intial_value, description);
+    }
+
+    function increment_counter() external  onlyOwner{
+        counter.number+=1;
+    }
+    function decrement_counter() external onlyOwner{
+        counter.number-=1;
+    }
+
+    function get_counter_value() external view returns(string memory){
+        return counter.description;
+    }
+}
